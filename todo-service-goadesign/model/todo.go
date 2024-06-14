c0ababc4e6b01e03951c5bbbf9545cc0210691c4
Christoph Kappel <christoph@unexist.dev>
2024-06-14 13:43:04 +0200

    Updated design

--- todo-service-goadesign/model/todo.go
+++ todo-service-goadesign/model/todo.go
@@ -13,21 +13,84 @@ package model
 
 import . "goa.design/model/dsl"
 
-var _ = Design("Getting Started", "This is a model of my software system.", func() {
-	var System = SoftwareSystem("Software System", "My software system.", func() {
+var backend_container interface{}
+
+var _ = Design("Todo design", "This is a design of the todo service.", func() {
+	Version("0.1")
+
+	var system = SoftwareSystem("Software System", "The software system.", func() {
 		Tag("system")
+
+		Container("Frontend Container", "A frontend container", "Angular", func() {
+			Tag("application")
+			URL("https://unexist.blog/todo")
+			Uses("Backend Container", "Uses", "HTTP", Synchronous)
+			Delivers("User", "Handles todo from", "HTTP", Asynchronous)
+		})
+
+		backend_container = Container("Backend Container", "A backend container", "GinTonic + REST", func() {
+			Tag("application")
+			URL("https://unexist.blog/todo")
+			Uses("Database Container", "Persists and retrieves data", "SQL", Synchronous)
+			Delivers("User", "Handles todo from", "HTTP", Asynchronous)
+			Component("Todo Service", "Golang")
+		})
+
+		Container("Database Container", "A database container", "Postgresql", func() {
+			Tag("infrastructure")
+			URL("https://unexist.blog/todo")
+		})
 	})
 
-	Person("User", "A user of my software system.", func() {
-		Uses(System, "Uses")
+	DeploymentEnvironment("Dev", func() {
+		DeploymentNode("Cloud", func() {
+			ContainerInstance("Software System/Frontend Container")
+			ContainerInstance("Software System/Backend Container")
+			ContainerInstance("Software System/Database Container")
+		})
+	})
+
+	Person("User", "A user of the software system.", func() {
+		Uses(system, "Uses")
+		Tag("person")
+	})
+
+	Person("Admin", "A user of the software system.", func() {
+		Uses(system, "Maintains")
 		Tag("person")
 	})
 
 	Views(func() {
-		SystemContextView(System, "SystemContext", "An example of a System Context diagram.", func() {
+		SystemLandscapeView("SystemLandscapeView", "A System Landscape View", func() {
+			Title("Overview of the system landscape")
 			AddAll()
 			AutoLayout(RankLeftRight)
 		})
+
+		SystemContextView(system, "SystemContext", "A System Context diagram.", func() {
+			Title("Overview of the system")
+			AddAll()
+			AutoLayout(RankLeftRight)
+		})
+
+		ContainerView(system, "ContainerView", "A Container View", func() {
+			Title("Overview of the containers")
+			AddAll()
+			AutoLayout(RankLeftRight)
+		})
+
+		ComponentView(backend_container, "ComponentView", "A Component View", func() {
+			Title("Overview of the components")
+			AddAll()
+			AutoLayout(RankLeftRight)
+		})
+
+		DeploymentView(Global, "Dev", "deployment", "A Deployment View", func() {
+			Title("Overview of the deployment on Dev")
+			AddAll()
+			AutoLayout(RankLeftRight)
+		})
+
 		Styles(func() {
 			ElementStyle("system", func() {
 				Background("#1168bd")
