78da5eb1339b8a271fe2d72adf4f92afb31b8482
Christoph Kappel <christoph@unexist.dev>
2024-06-14 10:01:06 +0200

    Added draft for goadesign

---
+++ todo-service-goadesign/go.mod
@@ -0,0 +1,18 @@
+module github.com/unexist/showcase-architecture-documentation
+
+go 1.22.1
+
+require (
+	github.com/dimfeld/httppath v0.0.0-20170720192232-ee938bf73598 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/manveru/faker v0.0.0-20171103152722-9fbc68a78c4d // indirect
+	github.com/sergi/go-diff v1.3.1 // indirect
+	goa.design/goa/v3 v3.16.2 // indirect
+	goa.design/model v1.9.8 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/tools v0.21.0 // indirect
+)
--- todo-service-goadesign/model/todo.go
+++ todo-service-goadesign/model/todo.go
@@ -1,32 +1,43 @@
+//
+// @package Showcase-Architecture-Documentation
+//
+// @file Todo model
+// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+// @version $Id$
+//
+// This program can be distributed under the terms of the Apache License v2.0.
+// See the file LICENSE for details.
+//
+
 package model
 
 import . "goa.design/model/dsl"
 
 var _ = Design("Getting Started", "This is a model of my software system.", func() {
-    var System = SoftwareSystem("Software System", "My software system.", func() {
-        Tag("system")
-    })
+	var System = SoftwareSystem("Software System", "My software system.", func() {
+		Tag("system")
+	})
 
-    Person("User", "A user of my software system.", func() {
-        Uses(System, "Uses")
-        Tag("person")
-    })
+	Person("User", "A user of my software system.", func() {
+		Uses(System, "Uses")
+		Tag("person")
+	})
 
-    Views(func() {
-        SystemContextView(System, "SystemContext", "An example of a System Context diagram.", func() {
-            AddAll()
-            AutoLayout(RankLeftRight)
-        })
-        Styles(func() {
-            ElementStyle("system", func() {
-                Background("#1168bd")
-                Color("#ffffff")
-            })
-            ElementStyle("person", func() {
-                Shape(ShapePerson)
-                Background("#08427b")
-                Color("#ffffff")
-            })
-        })
-    })
+	Views(func() {
+		SystemContextView(System, "SystemContext", "An example of a System Context diagram.", func() {
+			AddAll()
+			AutoLayout(RankLeftRight)
+		})
+		Styles(func() {
+			ElementStyle("system", func() {
+				Background("#1168bd")
+				Color("#ffffff")
+			})
+			ElementStyle("person", func() {
+				Shape(ShapePerson)
+				Background("#08427b")
+				Color("#ffffff")
+			})
+		})
+	})
 })
